23680cda00b971d4226d8e63073b3b8c36c63dfe
t-vaudry <tvaudryread@gmail.com>
2017-11-25 13:51:27 -0500

    Untabify, and using clear()

--- Assignment2/mapper.py
+++ Assignment2/mapper.py
@@ -11,17 +11,17 @@ current_year = None
 current_metric = 0
 
 for row in reader:
-	for column, value in row.items():
-		if column == "Location/Emplacement":
-			current_location = value
-		if column == "Collection Start/Début du prélèvement (UTC)":
-			current_year = value[:4]
-		if column == "7Be MDC/7Be CMD (mBq/m3)":
-			current_metric = value
+    for column, value in row.items():
+        if column == "Location/Emplacement":
+            current_location = value
+        if column == "Collection Start/Début du prélèvement (UTC)":
+            current_year = value[:4]
+        if column == "7Be MDC/7Be CMD (mBq/m3)":
+            current_metric = value
 
-	# write the results to STDOUT (standard output);
-	# what we output here will be the input for the
-	# reduce step, i.e. the input for reducer.py
-	#
-	# tab-delimited
-	print('(%s, %s)\t%s' % (current_location, current_year, current_metric))
+    # write the results to STDOUT (standard output);
+    # what we output here will be the input for the
+    # reduce step, i.e. the input for reducer.py
+    #
+    # tab-delimited
+    print('(%s, %s)\t%s' % (current_location, current_year, current_metric))
